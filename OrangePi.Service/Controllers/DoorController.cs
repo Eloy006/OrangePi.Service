9a33c8e2013794bfecc76f5c6638c97e3ff52b3a
Nikita Fenoshin <nikita.fenoshin@gmail.com>
2020-08-21 19:33:03 +0300

    Initial commit

---
+++ OrangePi.Service/Controllers/DoorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OrangePi.Service.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DoorController : ControllerBase
+    {
+        private readonly ILogger<DoorController> _logger;
+
+        public DoorController(ILogger<DoorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("open")]
+        public IActionResult Open()
+        {
+            _logger.LogInformation("Hello World!");
+
+            return Ok();
+        }
+    }
+}
